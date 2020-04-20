88009941e6623fcf50088880606a57430fa0a4e2
Madison Shippy <mshippy@cheyenne2.ib0.cheyenne.ucar.edu>
2020-04-20 13:45:15 -0600

    Daxpy with MPI

--- daxpyMPI.f90
+++ daxpyMPI.f90
@@ -11,39 +11,43 @@ program DaxpyProgram
   alpha = 2
 
   call MPI_INIT(error)
-  call MPI_COMM_SIZE(MPI_COMM_WORLD, procsize, error)
+  call MPI_COMM_SIZE(MPI_COMM_WORLD, p, error)
   call MPI_COMM_RANK(MPI_COMM_WORLD, rank, error)
 
   print *, 'process', rank, 'running'
 
-  allocate (x(n), y(n), z(n))
+  allocate x(n)
+  allocate y(n)
+  allocate z(n)
   call initialize (a, b, n)
 
-  if (MOD(n,p)/*0) then
+  if (MOD(n,p)/=0) then
     ops = n/p + 1
   else
     ops = n/p
   end if
 
-  if (id.eq.0) then
+  if (rank.eq.0) then
     print *, 'operations: ', ops
   end if
 
-  allocate(xpart(ops), ypart(ops), zpart(ops))
+  allocate xpart(ops)
+  allocate ypart(ops)
+  allocate zpart(ops)
 
 
-call MPI_Scatter(x, ops, MPI_DOUBLE_PRECISION, xpart, ops, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, error)
-call MPI_Scatter(y, ops, MPI_DOUBLE_PRECISION, ypart, ops, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, error)
-call MPI_Scatter(z, ops, MPI_DOUBLE_PRECISION, zpart, ops, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, error)
+call MPI_Scatter(x, ops, MPI_REAL, xpart, ops, MPI_REAL, 0, MPI_COMM_WORLD, error)
+call MPI_Scatter(y, ops, MPI_REAL, ypart, ops, MPI_REAL, 0, MPI_COMM_WORLD, error)
+call MPI_Scatter(z, ops, MPI_REAL, zpart, ops, MPI_REAL, 0, MPI_COMM_WORLD, error)
 
 print *,' process  ', id, 'section of vectors'
 
 call CPU_TIME(start)
 
 call MultAndAdd(alpha, xpart, ypart, zpart, ops, rank, n)
-call MPI_GATHER(zpart, ops, MPI_DOUBLE_PRECSION, z, ops, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, error)
+call MPI_GATHER(zpart, ops, MPI_REAL, z, ops, MPI_REAL, 0, MPI_COMM_WORLD, error)
 
-if (id.eq.0) then
+if (rank.eq.0) then
   do i = 1,n
     print *, z(i)
   end do
@@ -52,8 +56,15 @@ end if
 call CPU_TIME(finish)
 write (*,*) 'time of operation: ', finish-start, 's'
 
-deallocate(x, y, z, xpart, ypart, zpart)
+deallocate x
+deallocate y
+deallocate z
+deallocate xpart
+deallocate ypart
+deallocate zpart
+
 
 print *, 'exiting on process: ', rank
 
 call MPI_Finalize(error)
+end program
